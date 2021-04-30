83094a3d7eea3ec452fde0bcfd598c107e7d2d78
Jakub Molinski <jakub@molinski.dev>
2021-04-30 21:33:22 +0200

    Add task statement, split into pieces

---
+++ client_src/main.cc
@@ -0,0 +1,10 @@
+
+// ./screen-worms-client game_server [-n player_name] [-p n] [-i gui_server] [-r n]
+// game_server – adres (IPv4 lub IPv6) lub nazwa serwera gry
+//-n player_name – nazwa gracza, zgodna z opisanymi niżej wymaganiami
+//-p n – port serwera gry (domyślne 2021)
+//-i gui_server – adres (IPv4 lub IPv6) lub nazwa serwera obsługującego interfejs użytkownika (domyślnie localhost) -r n – port serwera obsługującego interfejs użytkownika (domyślnie 20210)
+// Do parsowania parametrów linii komend można użyć funkcji getopt z biblioteki standardowej: https://linux.die.net/man/3/getopt.
+
+int main(int argc, char *argv[]) {
+}
---
+++ server_src/main.cc
@@ -0,0 +1,15 @@
+
+// ./screen-worms-server [-p n] [-s n] [-t n] [-v n] [-w n] [-h n]
+//
+//  * `-p n` – numer portu (domyślnie `2021`)
+//  * `-s n` – ziarno generatora liczb losowych (opisanego poniżej, domyślnie
+//    wartość uzyskana przez wywołanie `time(NULL)`)
+//  * `-t n` – liczba całkowita wyznaczająca szybkość skrętu
+//    (parametr `TURNING_SPEED`, domyślnie `6`)
+//  * `-v n` – liczba całkowita wyznaczająca szybkość gry
+//    (parametr `ROUNDS_PER_SEC` w opisie protokołu, domyślnie `50`)
+//  * `-w n` – szerokość planszy w pikselach (domyślnie `640`)
+//  * `-h n` – wysokość planszy w pikselach (domyślnie `480`)
+
+int main(int argc, char *argv[]) {
+}
