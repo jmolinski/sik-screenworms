ad3f3b89a6fa56e5d3dbbd087022bd07edefb80c
Jakub Molinski <jakub@molinski.dev>
2021-05-20 19:55:04 +0200

    Add client logic for receiving messages from server

--- client_src/client.cpp
+++ client_src/client.cpp
@@ -10,18 +10,122 @@ constexpr unsigned TIMER_PFDS_IDX = 0;
 constexpr unsigned SERVER_SOCK_PFDS_IDX = 1;
 constexpr unsigned GUI_SOCK_PFDS_IDX = 2;
 constexpr unsigned long MSG_TO_SERVER_INTERVAL_MS = 5'000; // TODO ustawienie interwału 30 ms
+constexpr size_t INBOUND_SERVER_MESSAGE_BUFFER_SIZE = MAX_UDP_DATA_FIELD_SIZE + 10;
 
 Client::Client(ClientConfig conf)
     : config(std::move(conf)), serverSock({0, AF_INET6, SOCK_DGRAM, 0, 0, nullptr, nullptr, nullptr}, config.gameServer,
                                           config.serverPort, false),
       guiSock({0, AF_UNSPEC, SOCK_STREAM, 0, 0, nullptr, nullptr, nullptr}, config.guiServer, config.guiPort),
       messageToServer{{0}, 0, false}, sessionId(utils::timestampToUll(utils::getCurrentTimestamp())),
-      turnDirection(TurnDirection::straight) {
+      turnDirection(TurnDirection::straight), gameId(-1) {
     serverSock.connectPeer(serverSock.getAddrInfo());
     timerFd = utils::createArmedTimer(MSG_TO_SERVER_INTERVAL_MS * NS_IN_MS);
 }
 
+void Client::validateEvent(const Event &event) {
+    while (true) {
+        if (event.eventType == EventType::newGame) {
+            const auto &e = std::get<EventNewGame>(event.eventData);
+            if (e.parsedPlayers().size() > MAX_PLAYERS) {
+                break;
+            }
+        } else if (event.eventType == EventType::pixel) {
+            const auto &e = std::get<EventPixel>(event.eventData);
+            if (e.playerNumber > playersInGame || e.x > maxx || e.y > maxy) {
+                break;
+            }
+        } else if (event.eventType == EventType::playerEliminated) {
+            const auto &e = std::get<EventPlayerEliminated>(event.eventData);
+            if (e.playerNumber > playersInGame) {
+                break;
+            }
+        }
+        return;
+    }
+    std::cerr << "Received corrupted data from server with correct crc32. Failing \n" << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+void Client::readInEventsListOfCurrentGame(const ServerToClientMessage &msg) {
+    for (const Event &event : msg.events) {
+        validateEvent(event);
+        if (events.find(event.eventNo) != events.end()) {
+            events.insert({event.eventNo, event});
+        }
+    }
+}
+
+void Client::processMessageFromServerWithMismatchedGameId(const ServerToClientMessage &msg) {
+    if (seenGameIds.find(msg.gameId) != seenGameIds.end()) {
+        // A strayed packet describing one of the previous games.
+        return;
+    }
+
+    // A packet describing a new game.
+    gameId = msg.gameId;
+    playersInGame = 0;
+    nextWantedEventNo = 0;
+    nextEventToSendNo = 0;
+    events.clear();
+
+    // Try to find NEW_GAME event.
+    for (const Event &event : msg.events) {
+        if (event.eventType == EventType::newGame) {
+            validateEvent(event);
+            const auto &newGame = std::get<EventNewGame>(event.eventData);
+            maxx = newGame.maxx;
+            maxy = newGame.maxy;
+            playerNames = newGame.parsedPlayers(); // TODO throws
+            playersInGame = static_cast<uint8_t>(playerNames.size());
+            readInEventsListOfCurrentGame(msg);
+            return;
+        }
+    }
+}
+
+void Client::processMessageFromServer(const ServerToClientMessage &msg) {
+    if (msg.gameId != gameId) {
+        processMessageFromServerWithMismatchedGameId(msg);
+    } else {
+        readInEventsListOfCurrentGame(msg);
+    }
+
+    while (events.find(nextWantedEventNo) != events.end()) {
+        nextWantedEventNo++;
+    }
+    while (nextEventToSendNo < nextWantedEventNo) {
+        guiSock.writeData(eventToMessageForGui(events.find(nextEventToSendNo)->second, playerNames));
+        nextEventToSendNo++;
+    }
+}
+
 void Client::readMessageFromServer() {
+    static unsigned char buf[INBOUND_SERVER_MESSAGE_BUFFER_SIZE];
+
+    errno = 0;
+    ssize_t numbytes = recv(serverSock.getFd(), buf, INBOUND_SERVER_MESSAGE_BUFFER_SIZE, MSG_DONTWAIT);
+    if (numbytes == -1) {
+        if (errno != EAGAIN && errno != EWOULDBLOCK) {
+            perror("Error in recvfrom. Skipping this datagram processing.");
+        }
+        return;
+    }
+
+    auto bytesRead = static_cast<size_t>(numbytes);
+    if (bytesRead > MAX_UDP_DATA_FIELD_SIZE) {
+        // Discarding an invalid datagram.
+        return;
+    }
+
+    std::cout << "DEBUG got packet from server" << std::endl;
+
+    try {
+        ServerToClientMessage m(buf, bytesRead);
+        processMessageFromServer(m);
+    } catch (const EncoderDecoderError &e) {
+        std::cerr << "Error in server message decoding. Skipping this datagram processing." << std::endl;
+        return;
+    }
 }
 
 void Client::sendMessageToServer() {
--- client_src/client.h
+++ client_src/client.h
@@ -6,6 +6,7 @@
 #include "../common/time_utils.h"
 #include "client_config.h"
 #include <unordered_map>
+#include <unordered_set>
 
 namespace {
     constexpr size_t MSG_TO_SERVER_BUFFER_SIZE = 50;
@@ -17,24 +18,31 @@ struct MessageToServer {
     bool ready;
 };
 
-struct DecodedEvent {
-    std::string guiFormat;
-    uint32_t eventNo;
-};
-
 class Client {
+    // Client config & persistent objects like sockets.
     ClientConfig config;
     UdpSocket serverSock;
     TcpSocket guiSock;
     timer_fd_t timerFd;
 
+    // Fields relating to client->server communication.
     MessageToServer messageToServer;
     uint64_t sessionId;
     TurnDirection turnDirection;
 
-    uint32_t nextWantedEventNo;  // TODO ?
-    uint32_t nextEventToSendNo;  // TODO ?
-    std::unordered_map<uint32_t, DecodedEvent> events;
+    // Current game events & fields relating to client->gui communication.
+    std::unordered_map<uint32_t, Event> events;
+    uint32_t nextWantedEventNo; // TODO ?
+    uint32_t nextEventToSendNo; // TODO ?
+
+    // Registry of played games, current gameId.
+    std::unordered_set<uint32_t> seenGameIds;
+    int64_t gameId;
+    uint8_t playersInGame;
+
+    // Current game settings.
+    uint32_t maxx, maxy;
+    std::unordered_map<uint8_t, std::string> playerNames;
 
   public:
     explicit Client(ClientConfig config);
@@ -45,6 +53,10 @@ class Client {
     void enqueueMessageToServer();
     void sendMessageToServer();
     void readUpdateFromGui();
+    void processMessageFromServer(const ServerToClientMessage &);
+    void processMessageFromServerWithMismatchedGameId(const ServerToClientMessage &);
+    void readInEventsListOfCurrentGame(const ServerToClientMessage &msg);
+    void validateEvent(const Event &event);
 };
 
 #endif // SIK_NETWORMS_CLIENT_H
